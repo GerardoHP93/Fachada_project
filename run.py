6306ee34910791ead65d30019312d80b6d6ddfc4
GerardoHP93 <gerardohp93@gmail.com>
2025-03-05 01:43:50 -0600

    Cambiando el puerto para rende

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask_cors import CORS
 from app import create_app
 
 app = create_app()
+CORS(app)  # Habilita CORS para toda la API
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)  # Asegura que Render lo ejecute en el puerto correcto
